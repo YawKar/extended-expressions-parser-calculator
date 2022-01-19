1478cdb615da006d623ba3960703c2bbc26319aa
Karpov Vadim <fenixball12345@gmail.com>
2022-01-20 00:32:17 +0300

    Описал все вершины, уже работает на простых числах Надо поправить: 1. Неправильный порядок операций (precedence) 2. SegFault при вызове функций (не понятно, почему)

--- nodes.h
+++ nodes.h
@@ -5,7 +5,7 @@
 enum NodeType {
     OPERATOR_NODE,       // ^; *; /; +; -; 
     NUMBER_NODE,         // 1; 1.2; -1.1; 0.012; 50; 24;
-    CONSTANT_NODE,       // e; PI; j;
+    CONSTANT_NODE,       // e; PI;
     STDFUNCTION_NODE,    // cos; sin; tg; log; ln; sqrt; pow; abs; exp; real; imag; mag; phase;
     VARIABLE_NODE        // alpha = sin(PI*2.5+sqrt(5)); beta = sqrt(sin(1)^2 <or> sqrt((sin(1))^2);
 };
@@ -49,8 +49,7 @@ struct NumberNode {
 // CONSTANT NODE
 enum ConstantType {
     EULER_NUMBER_CONSTANT,   // e;
-    PI_NUMBER_CONSTANT,      // PI;
-    J_NUMBER_CONSTANT        // j;
+    PI_NUMBER_CONSTANT       // PI;
 };
 typedef enum ConstantType ConstantType;
 
@@ -81,7 +80,7 @@ typedef enum StdFunctionType StdFunctionType;
 typedef struct StdFunctionNode StdFunctionNode;
 struct StdFunctionNode {
     StdFunctionType stdFunctionType;
-    OperandsContainer operands;
+    OperandsContainer* operands;
 };
 
 // VARIABLE NODE
--- test.c
+++ test.c
@@ -15,6 +15,15 @@ int is_forbidden_char (char ch) {
     return ch == '\0';
 }
 
+int is_operator (char ch) {
+    char* operators = "^*/+-\0";
+    int operators_len = strlen(operators);
+    for (int i = 0; i < operators_len; ++i)
+        if (operators[i] == ch)
+            return 1;
+    return 0;
+}
+
 char* prettify_expression (char* expression) {
     int exps_len = strlen(expression);
     int final_len = 0;
@@ -207,6 +216,209 @@ long double parse_number (char* expression, int left, int right) {
     return res;
 }
 
+int is_constant (char* expression, int left, int right) {
+    char* constants[] = {
+        "PI\0", "e\0"
+    };
+    int constantsNumber = 2;
+    for (int i = 0; i < constantsNumber; ++i) {
+        int constLen = strlen(constants[i]);
+        if (constLen != right - left)
+            continue;
+        int areEqual = 1;
+        for (int j = 0; j < constLen; ++j) {
+            areEqual &= constants[i][j] == expression[left + j];
+        }
+        if (areEqual)
+            return 1;
+    }
+    return 0;
+}
+
+ConstantType get_constant_type (char* expression, int left, int right) {
+    char* constants[] = {
+        "PI\0", "e\0"
+    };
+    int constantsNumber = 2;
+    for (int i = 0; i < constantsNumber; ++i) {
+        int constLen = strlen(constants[i]);
+        if (constLen != right - left)
+            continue;
+        int areEqual = 1;
+        for (int j = 0; j < constLen; ++j) {
+            areEqual &= constants[i][j] == expression[left + j];
+        }
+        if (areEqual) {
+            switch (i) {
+                case 0:
+                    return PI_NUMBER_CONSTANT;
+                case 1:
+                    return EULER_NUMBER_CONSTANT;
+            }
+        }
+    }
+    return PI_NUMBER_CONSTANT;
+}
+
+int is_stdfunction(char* expression, int left, int right) {
+    char* stdFunctions[] = {
+        "cos\0",
+        "sin\0",
+        "tg\0",
+        "ctg\0",
+        "log\0",
+        "ln\0",
+        "sqrt\0",
+        "pow\0",
+        "abs\0",
+        "exp\0",
+        "real\0",
+        "imag\0",
+        "mag\0",
+        "phase\0"
+    };
+    int openBracketIndex = -1;
+    for (int i = left; i < right; ++i) {
+        if (expression[i] == '(') {
+            openBracketIndex = i;
+            break;
+        }
+    }
+    if (openBracketIndex == -1)
+        return 0;
+    int stdFuncNumber = 14;
+    for (int funcIndex = 0; funcIndex < stdFuncNumber; ++funcIndex) {
+        int stdFuncLen = strlen(stdFunctions[funcIndex]);
+        if (stdFuncLen != openBracketIndex)
+            continue;
+        int areEqual = 1;
+        for (int j = 0; j < stdFuncLen; ++j)
+            if (stdFunctions[funcIndex][j] != expression[left + j]) {
+                areEqual = 0;
+                break;
+            }
+        if (areEqual) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+StdFunctionType get_stdfunc_type(char* expression, int left, int right) {
+    char* stdFunctions[] = {
+        "cos\0",
+        "sin\0",
+        "tg\0",
+        "ctg\0",
+        "log\0",
+        "ln\0",
+        "sqrt\0",
+        "pow\0",
+        "abs\0",
+        "exp\0",
+        "real\0",
+        "imag\0",
+        "mag\0",
+        "phase\0"
+    };
+    int openBracketIndex = -1;
+    for (int i = left; i < right; ++i) {
+        if (expression[i] == '(') {
+            openBracketIndex = i;
+            break;
+        }
+    }
+    int stdFuncNumber = 14;
+    for (int funcIndex = 0; funcIndex < stdFuncNumber; ++funcIndex) {
+        int stdFuncLen = strlen(stdFunctions[funcIndex]);
+        if (stdFuncLen != openBracketIndex)
+            continue;
+        int areEqual = 1;
+        for (int j = 0; j < stdFuncLen; ++j)
+            if (stdFunctions[funcIndex][j] != expression[left + j]) {
+                areEqual = 0;
+                break;
+            }
+        if (areEqual) {
+            switch (funcIndex) {
+                case 0:
+                    return COS_FUNC;
+                case 1:
+                    return SIN_FUNC;
+                case 2:
+                    return TG_FUNC;
+                case 3:
+                    return CTG_FUNC;
+                case 4:
+                    return LOG_FUNC;
+                case 5:
+                    return LN_FUNC;
+                case 6:
+                    return SQRT_FUNC;
+                case 7:
+                    return POW_FUNC;
+                case 8:
+                    return ABS_FUNC;
+                case 9:
+                    return EXP_FUNC;
+                case 10:
+                    return REAL_FUNC;
+                case 11:
+                    return IMAG_FUNC;
+                case 12:
+                    return MAG_FUNC;
+                case 13:
+                    return PHASE_FUNC;
+            }
+        }
+    }
+    return COS_FUNC;
+}
+
+void add_operand_to_operands_container(BaseNode* operand, OperandsContainer* opContainer) {
+    BaseNode** newOperands = (BaseNode**) malloc(sizeof(BaseNode*) * (opContainer->operandsNumber + 1));
+    for (int i = 0; i < opContainer->operandsNumber; ++i) {
+        newOperands[i] = opContainer->operands[i];
+    }
+    newOperands[opContainer->operandsNumber] = operand;
+    free(opContainer->operands);
+    opContainer->operands = newOperands;
+    ++opContainer->operandsNumber;
+}
+
+void parse_expression (BaseNode* root, char* expression, int left, int right);
+
+void parse_arguments (char* expression, int left, int right, OperandsContainer* operands) {
+    int openBracketIndex = -1;
+    for (int i = left; i < right; ++i) {
+        if (expression[i] == '(') {
+            openBracketIndex = i;
+            break;
+        }
+    }
+    int lastComma = openBracketIndex;
+    int curLevel = 0;
+    for (int i = openBracketIndex + 1; i < right - 1; ++i) {
+        if (expression[i] == '(') {
+            ++curLevel;
+            continue;
+        }
+        if (expression[i] == ')') {
+            --curLevel;
+            continue;
+        }
+        if (curLevel != 0)
+            continue;
+        if (expression[i] == ',') {
+            // FOUND ARGUMENT
+            BaseNode* newOperand = new_base_node();
+            add_operand_to_operands_container(newOperand, operands);
+            parse_expression(newOperand, expression, lastComma + 1, i);
+            lastComma = i;
+        }
+    }
+}
+
 void parse_expression (BaseNode* root, char* expression, int left, int right) {
     if (is_bracketed(expression, left, right)) {
         int brackets_count = count_ends_brackets(expression, left, right);
@@ -220,34 +432,44 @@ void parse_expression (BaseNode* root, char* expression, int left, int right) {
         root->nodeType = OPERATOR_NODE;
         switch (max_oper) {
             case POWER_OP:
-                OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
-                opNode->operatorType = POWER_OP;
-                opNode->operands = new_operands_container();
-                root->realNode = (void*) opNode;
+                {
+                    OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
+                    opNode->operatorType = POWER_OP;
+                    opNode->operands = new_operands_container();
+                    root->realNode = (void*) opNode;
+                }
                 break;
             case MULTIPLY_OP:
-                OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
-                opNode->operatorType = MULTIPLY_OP;  
-                opNode->operands = new_operands_container();
-                root->realNode = (void*) opNode;
+                {
+                    OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
+                    opNode->operatorType = MULTIPLY_OP;  
+                    opNode->operands = new_operands_container();
+                    root->realNode = (void*) opNode;
+                }
                 break;
             case DIVIDE_OP:
-                OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
-                opNode->operatorType = DIVIDE_OP;  
-                opNode->operands = new_operands_container();
-                root->realNode = (void*) opNode;
+                {
+                    OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
+                    opNode->operatorType = DIVIDE_OP;  
+                    opNode->operands = new_operands_container();
+                    root->realNode = (void*) opNode;
+                }
                 break;
             case PLUS_OP:
-                OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
-                opNode->operatorType = PLUS_OP;  
-                opNode->operands = new_operands_container();
-                root->realNode = (void*) opNode;
+                {
+                    OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
+                    opNode->operatorType = PLUS_OP;  
+                    opNode->operands = new_operands_container();
+                    root->realNode = (void*) opNode;
+                }
                 break;
             case MINUS_OP:
-                OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
-                opNode->operatorType = MINUS_OP;  
-                opNode->operands = new_operands_container();
-                root->realNode = (void*) opNode;
+                {
+                    OperatorNode* opNode = (OperatorNode*) malloc(sizeof(OperatorNode));
+                    opNode->operatorType = MINUS_OP;  
+                    opNode->operands = new_operands_container();
+                    root->realNode = (void*) opNode;
+                }
                 break;
         }
         OperatorNode* opNode = (OperatorNode*) root->realNode;
@@ -264,14 +486,138 @@ void parse_expression (BaseNode* root, char* expression, int left, int right) {
         NumberNode* numNode = (NumberNode*) malloc(sizeof(NumberNode));
         numNode->number = number;
         root->realNode = (void*) numNode;
+    } else if (is_constant(expression, left, right)) {
+        // A constant: e, PI;
+        ConstantNode* consNode = (ConstantNode*) malloc(sizeof(ConstantNode));
+        consNode->constantType = get_constant_type(expression, left, right);
+        root->realNode = (void*) consNode;
+    } else if (is_stdfunction(expression, left, right)) {
+        // A std function: cos, sin...
+        StdFunctionNode* stdFuncNode = (StdFunctionNode*) malloc(sizeof(StdFunctionNode));
+        stdFuncNode->stdFunctionType = get_stdfunc_type(expression, left, right);
+        stdFuncNode->operands = new_operands_container();
+        root->realNode = (void*) stdFuncNode;
+        parse_arguments(expression, left, right, stdFuncNode->operands);
     }
 }
 
+long double eval_base_node (BaseNode* root);
+
+long double eval_operator_node (OperatorNode* opNode) {
+    long double arg1;
+    long double arg2;
+    switch (opNode->operatorType) {
+        case POWER_OP:
+            arg1 = eval_base_node(opNode->operands->operands[0]);
+            arg2 = eval_base_node(opNode->operands->operands[1]);
+            return pow(arg1, arg2);
+        case MULTIPLY_OP:
+            arg1 = eval_base_node(opNode->operands->operands[0]);
+            arg2 = eval_base_node(opNode->operands->operands[1]);
+            return arg1 * arg2;
+        case DIVIDE_OP:
+            arg1 = eval_base_node(opNode->operands->operands[0]);
+            arg2 = eval_base_node(opNode->operands->operands[1]);
+            return arg1 / arg2;
+        case PLUS_OP:
+            arg1 = eval_base_node(opNode->operands->operands[0]);
+            arg2 = eval_base_node(opNode->operands->operands[1]);
+            return arg1 + arg2;
+        case MINUS_OP:
+            arg1 = eval_base_node(opNode->operands->operands[0]);
+            arg2 = eval_base_node(opNode->operands->operands[1]);
+            return arg1 - arg2;
+    }
+    return 0L;
+}
+
+long double eval_number_node (NumberNode* numNode) {
+    return numNode->number;
+}
+
+long double eval_constant_node (ConstantNode* consNode) {
+    switch (consNode->constantType) {
+        case EULER_NUMBER_CONSTANT:
+            return exp(1.0);
+        case PI_NUMBER_CONSTANT:
+            return M_PI;
+    }
+    return 0L;
+}
+
+long double logWithBase (long double base, long double value) {
+    return log(value) / log(base);
+}
+
+long double eval_stdfunction_node (StdFunctionNode* stdFunctionNode) {
+    long double arg1;
+    long double arg2;
+    switch (stdFunctionNode->stdFunctionType) {
+        case COS_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return cos(arg1);
+        case SIN_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return sin(arg1);
+        case TG_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return tan(arg1);
+        case CTG_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return cos(arg1) / sin(arg1);
+        case LOG_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            arg2 = eval_base_node(stdFunctionNode->operands->operands[1]);
+            return logWithBase(arg1, arg2);
+        case LN_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return log(arg1);
+        case SQRT_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return sqrt(arg1);
+        case POW_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            arg2 = eval_base_node(stdFunctionNode->operands->operands[1]);
+            return pow(arg1, arg2);
+        case ABS_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return fabs(arg1);
+        case EXP_FUNC:
+            arg1 = eval_base_node(stdFunctionNode->operands->operands[0]);
+            return exp(arg1);
+        case REAL_FUNC:
+            return 0L;
+        case IMAG_FUNC:
+            return 0L;
+        case MAG_FUNC:
+            return 0L;
+        case PHASE_FUNC:
+            return 0L;
+    }
+    return 0L;
+}
+
+long double eval_base_node (BaseNode* root) {
+    switch (root->nodeType) {
+        case OPERATOR_NODE:
+            return eval_operator_node((OperatorNode*)root->realNode);
+        case NUMBER_NODE:
+            return eval_number_node((NumberNode*)root->realNode);
+        case CONSTANT_NODE:
+            return eval_constant_node((ConstantNode*)root->realNode);
+        case STDFUNCTION_NODE:
+            return eval_stdfunction_node((StdFunctionNode*)root->realNode);
+    }
+    return 0;
+}
+
 int main () {
+    long double res=  pow(2L, 10L);
     char* raw_expression = get_line();
     char* expression = prettify_expression(raw_expression);
     int expression_length = strlen(expression);
     BaseNode* root = (BaseNode*) malloc(sizeof(BaseNode));
     parse_expression(root, expression, 0, expression_length);
+    printf("Evaluate: %llf", eval_base_node(root));
     return 0;
 }
